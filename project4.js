d7bf4ec5070b42b9e6c9a90b90c6d1a3f2021f4d
avishkushwah <avishkushwah50@gmail.com>
2022-10-11 12:00:26 +0530

    added

---
+++ project4.js
@@ -0,0 +1,10 @@
+let name = "Ajay Sharma";
+let school = "Lions convent high secondary school";
+let grade = "11th";
+let section ="Biology";
+let roll_no = "110566";
+let marks_bio = "75%";
+let marks_physics = "80%";
+let marks_chemistery = "55%";
+
+console.log(name,grade,section,roll_no,marks_bio,marks_physics,marks_chemistery,school);
